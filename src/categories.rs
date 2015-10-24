2804320bd60a53bf9cb0548f189c7bafccdb81dd
Zachary Bush <zach@zmbush.com>
2015-10-24 14:44:27 -0700

    Reformat categories file

--- src/categories.rs
+++ src/categories.rs
@@ -2,11 +2,11 @@ use phf::{Map, Set};
 
 pub static CATEGORIES: Map<&'static str, Set<&'static str>> = phf_map! {
     "Auto Payment" => phf_set![],
-    "Automobile" => phf_set!["Auto & Transport", "Service & Parts", "Gas & Fuel",
-                             "Parking"],
-    "Bills" => phf_set!["Bills & Utilities", "BillsCable", "BillsElectric", "BillsPhone",
-                        "BillsRent", "Internet", "Mortgage & Rent", "Utilities", "Security",
-                        "Mobile Phone"],
+    "Automobile" => phf_set![
+        "Auto & Transport", "Service & Parts", "Gas & Fuel", "Parking"],
+    "Bills" => phf_set![
+        "Bills & Utilities", "BillsCable", "BillsElectric", "BillsPhone", "BillsRent", "Internet",
+        "Mortgage & Rent", "Utilities", "Security", "Mobile Phone"],
     "Business" => phf_set!["Advertising","Business Services", "Office Supplies"],
     "Cash" => phf_set!["ATM", "Cash & ATM"],
     "Charity" => phf_set![],
@@ -14,13 +14,16 @@ pub static CATEGORIES: Map<&'static str, Set<&'static str>> = phf_map! {
     "Entertainment" => phf_set!["Movies & DVDs", "Music", "Arts", "Amusement"],
     "Fees" => phf_set!["Fees & Charges", "Finance Charge", "Bank Fee", "Shipping", "ATM Fee"],
     "Financial" => phf_set![],
-    "Food & Dining" => phf_set!["Alcohol & Bars", "Fast Food", "Food Trucks", "DiningOut",
-                                "Delivery/Takeout", "Coffee Shops", "Restaurants", "Seamless"],
+    "Food & Dining" => phf_set![
+        "Alcohol & Bars", "Fast Food", "Food Trucks", "DiningOut", "Delivery/Takeout",
+        "Coffee Shops", "Restaurants", "Seamless"],
     "Gifts" => phf_set!["Gift"],
     "Groceries" => phf_set![],
     "Health" => phf_set!["Pharmacy", "HealthDental", "Health & Fitness", "Dentist", "Doctor", "Gym"],
     "Hobbies" => phf_set!["Books", "Books & Magazines", "Conventions"],
-    "Household" => phf_set!["Furnishings", "Home Improvement", "Home Services", "Lawn & Garden", "Pets", "Home Supplies"],
+    "Household" => phf_set![
+        "Furnishings", "Home Improvement", "Home Services", "Lawn & Garden", "Pets",
+        "Home Supplies"],
     "Income" => phf_set!["Wages", "Paycheck", "Rental Income"],
     "Insurance" => phf_set!["Auto Insurance", "BillsInsurance"],
     "Interest" => phf_set!["Interest Income", "Dividends"],
@@ -35,9 +38,9 @@ pub static CATEGORIES: Map<&'static str, Set<&'static str>> = phf_map! {
     "Television" => phf_set![],
     "Transfers" => phf_set!["Transfer", "Credit Card Payment"],
     "Travel" => phf_set!["Hotel", "Public Transportation", "Rental Car & Taxi"],
-    "Uncategorized" => phf_set!["Transfer", "Check", "CreditMiscellaneous", "CreditNone",
-                                "CreditTransfer", "DebitMiscellaneous", "DebitNone",
-                                "DebitTransfer", "Reimbursement"],
+    "Uncategorized" => phf_set![
+        "Transfer", "Check", "CreditMiscellaneous", "CreditNone", "CreditTransfer",
+        "DebitMiscellaneous", "DebitNone", "DebitTransfer", "Reimbursement"],
     "Hide" => phf_set!["Hide from Budgets & Trends", "Pending"]
 };
 
