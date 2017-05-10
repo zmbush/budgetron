2fc158814245779e89055a3b76bad655ef5cf8a4
Zach Bush <zach@zmbush.com>
2017-05-09 22:12:38 -0700

    Move export parsing code to transactor

--- budgetronlib/src/error.rs
+++ budgetronlib/src/error.rs
@@ -1,16 +1,24 @@
 use csv;
 use std::convert::From;
+use toml;
 
 #[derive(Debug)]
 pub enum BudgetError {
     CSVError(csv::Error),
     NoCategoryFoundError(String),
+    TomlDeError(toml::de::Error),
     NoTransactionError,
 }
 
 impl From<csv::Error> for BudgetError {
     fn from(e: csv::Error) -> BudgetError {
         BudgetError::CSVError(e)
+    }
+}
+
+impl From<toml::de::Error> for BudgetError {
+    fn from(e: toml::de::Error) -> BudgetError {
+        BudgetError::TomlDeError(e)
     }
 }
 
