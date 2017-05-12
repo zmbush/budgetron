d9081bd0cf4186d815569078257904280e9d2706
Zach Bush <zach@zmbush.com>
2017-05-11 22:41:58 -0700

    Start collator

--- transactor/src/mint.rs
+++ transactor/src/mint.rs
@@ -4,6 +4,21 @@ use budgetronlib::fintime::Date;
 use generic::{Transaction, Person, TransactionType, Genericize};
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all="lowercase")]
+pub enum MintTransactionType {
+    Debit,
+    Credit,
+}
+
+impl Into<TransactionType> for MintTransactionType {
+    fn into(self) -> TransactionType {
+        match self {
+            MintTransactionType::Debit => TransactionType::Debit,
+            MintTransactionType::Credit => TransactionType::Credit,
+        }
+    }
+}
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct MintExport {
     date: Date,
@@ -12,7 +27,7 @@ pub struct MintExport {
     original_description: String,
     amount: f64,
     #[serde(rename="Transaction Type")]
-    transaction_type: TransactionType,
+    transaction_type: MintTransactionType,
     category: String,
     #[serde(rename="Account Name")]
     account_name: String,
@@ -28,12 +43,13 @@ impl Genericize for MintExport {
                description: self.description,
                original_description: self.original_description,
                amount: self.amount,
-               transaction_type: self.transaction_type,
+               transaction_type: self.transaction_type.into(),
                category: cfg.find_category(&self.category)?.to_owned(),
                original_category: self.category,
                account_name: self.account_name,
                labels: self.labels,
                notes: self.notes,
+               transfer_destination_account: None,
            })
 
     }
