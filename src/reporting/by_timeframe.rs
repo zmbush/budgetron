aebfebe863158d765eea850ed9b1d2e848f6481b
Zach Bush <zach@zmbush.com>
2017-05-25 21:48:33 -0700

    Add more reports

---
+++ src/reporting/by_timeframe.rs
@@ -0,0 +1,89 @@
+use budgetronlib::fintime::{Date, Timeframe};
+use loading::Transaction;
+use reporting::Reporter;
+use std::collections::BTreeMap;
+use std::fmt;
+
+pub struct ByTimeframe<'a, T>
+    where T: 'a + Reporter
+{
+    inner: &'a T,
+    timeframe: Timeframe,
+}
+
+impl<'a, T> ByTimeframe<'a, T>
+    where T: 'a + Reporter
+{
+    pub fn new(inner: &'a T, timeframe: Timeframe) -> Self {
+        ByTimeframe { inner, timeframe }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct ByTimeframeReport<T> {
+    timeframe: Timeframe,
+    by_timeframe: BTreeMap<Date, T>,
+}
+
+impl<T> ByTimeframeReport<T>
+    where T: fmt::Display
+{
+    pub fn print(&self) {
+        println!("{}", self)
+    }
+}
+
+impl<T> fmt::Display for ByTimeframeReport<T>
+    where T: fmt::Display
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (key, value) in &self.by_timeframe {
+            writeln!(f,
+                     "For the transactions in {}-{}",
+                     key,
+                     key + self.timeframe - Timeframe::Days(1))?;
+            writeln!(f, "{}", value)?;
+        }
+        Ok(())
+    }
+}
+
+impl<'a, T> Reporter for ByTimeframe<'a, T>
+    where T: Reporter
+{
+    type OutputType = ByTimeframeReport<T::OutputType>;
+
+    fn report<'b, I>(&self, transactions: I) -> ByTimeframeReport<T::OutputType>
+        where I: Iterator<Item = &'b Transaction>
+    {
+        let mut transactions: Vec<_> = transactions.collect();
+        let mut date = transactions
+            .get(0)
+            .map(|t| t.date)
+            .clone()
+            .unwrap_or_else(|| Date::ymd(2000, 1, 1));
+
+        match self.timeframe {
+            Timeframe::Days(_) => {},
+            Timeframe::Weeks(_) => date.align_to_week(),
+            Timeframe::Months(_) => date.align_to_month(),
+            Timeframe::Quarters(_) => date.align_to_quarter(),
+            Timeframe::Years(_) => date.align_to_year(),
+        }
+
+        let mut by_timeframe = BTreeMap::new();
+        while transactions.len() > 0 {
+            let (current, remaining): (Vec<_>, Vec<_>) =
+                transactions
+                    .into_iter()
+                    .partition(|t| t.date >= date && t.date < date + self.timeframe);
+            transactions = remaining;
+            by_timeframe.insert(date, self.inner.report(current.into_iter()));
+            date += self.timeframe;
+        }
+        ByTimeframeReport {
+            by_timeframe,
+            timeframe: self.timeframe,
+        }
+    }
+}
--- src/reporting/mod.rs
+++ src/reporting/mod.rs
@@ -1,14 +1,41 @@
+use budgetronlib::fintime::Timeframe;
 use loading::Transaction;
+use reporting::by_account::ByAccount;
+use reporting::by_timeframe::ByTimeframe;
+use serde;
 use std::fmt;
 
-pub trait Reporter {
+pub trait Reporter: Sized {
     type OutputType;
 
-    fn report(&self, transactions: &Vec<Transaction>) -> Self::OutputType;
+    fn report<'a, I>(&self, transactions: I) -> Self::OutputType
+        where I: Iterator<Item = &'a Transaction>;
+
+    fn by_week(&self) -> ByTimeframe<Self> {
+        ByTimeframe::new(self, Timeframe::Weeks(1))
+    }
+
+    fn by_month(&self) -> ByTimeframe<Self> {
+        ByTimeframe::new(self, Timeframe::Months(1))
+    }
+
+    fn by_quarter(&self) -> ByTimeframe<Self> {
+        ByTimeframe::new(self, Timeframe::Quarters(1))
+    }
+
+    fn for_account(&self, account: String) -> ByAccount<Self> {
+        ByAccount::new(self, account)
+    }
 }
 
+pub trait Report: fmt::Display + serde::Serialize {}
+
+mod by_timeframe;
+mod by_account;
 mod net_worth;
 mod database;
+mod cashflow;
 
+pub use reporting::cashflow::Cashflow;
 pub use reporting::database::Database;
 pub use reporting::net_worth::NetWorth;
